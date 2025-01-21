d2799776aa5975416397513af82fdf32ef1e5734
Natan Hari Pamungkas <natanhp@protonmail.com>
2025-01-22 02:29:58 +0700

    Create test delete l swipe
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -35,6 +35,24 @@ func (suite *SchedulerTestSuite) TestResetSwipeNumber() {
 	assert.Equal(suite.T(), true, user.NextSwipeReset.After(time.Now()))
 }
 
+func (suite *SchedulerTestSuite) TestDeleteLSwipes() {
+	for i := 0; i < 9; i++ {
+		lSwipe := models.LSwipe{
+			UserID:   uint(i) + 1,
+			LSwipeID: uint(i) + 2,
+			DeleteOn: time.Now().AddDate(0, 0, -1),
+		}
+
+		DBTest.Create(&lSwipe)
+	}
+
+	scheduler.DeleteLSwipes()
+	var lSwipes []models.LSwipe
+	suite.DB.Find(&lSwipes)
+
+	assert.Equal(suite.T(), true, len(lSwipes) == 0)
+}
+
 func TestSchedulerTestSuite(t *testing.T) {
 	suite.Run(t, new(SchedulerTestSuite))
 }
