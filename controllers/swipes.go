fe4d868f1efcbe732e2dec28221334592e0d3eda
Natan Hari Pamungkas <natanhp@protonmail.com>
2025-01-22 04:13:27 +0700

    Refactor swipes
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- controllers/swipes.go
+++ controllers/swipes.go
@@ -24,44 +24,8 @@ func Right(c *gin.Context) {
 
 	var swiper models.User
 	config.DB.First(&swiper, userID)
-	var existingUser models.User
-	config.DB.First(&existingUser, swipe.RSwipeID)
 
-	if existingUser.ID == 0 {
-		c.JSON(400, gin.H{
-			"error": "User not found",
-		})
-
-		return
-	}
-
-	if swiper.SwipeNum <= 0 {
-		c.JSON(400, gin.H{
-			"error": "Out of swipes",
-		})
-
-		return
-	}
-
-	var existingRSwipe models.RSwipe
-	config.DB.Where("user_id = ? AND r_swipe_id = ?", userID, swipe.RSwipeID).First(&existingRSwipe)
-
-	if existingRSwipe.UserID != 0 {
-		c.JSON(400, gin.H{
-			"error": "Already swiped",
-		})
-
-		return
-	}
-
-	var existingLSwipe models.LSwipe
-	config.DB.Where("user_id = ? AND l_swipe_id = ?", userID, swipe.RSwipeID).First(&existingLSwipe)
-
-	if existingLSwipe.UserID != 0 {
-		c.JSON(400, gin.H{
-			"error": "Already swiped",
-		})
-
+	if isSwiped(userID, swipe.RSwipeID, swiper, c) {
 		return
 	}
 
@@ -76,6 +40,50 @@ func Right(c *gin.Context) {
 	})
 }
 
+func isSwiped(userID uint, swipedID uint, swiper models.User, c *gin.Context) bool {
+	var existingUser models.User
+	config.DB.First(&existingUser, swipedID)
+
+	if existingUser.ID == 0 {
+		c.JSON(400, gin.H{
+			"error": "User not found",
+		})
+
+		return true
+	}
+
+	if swiper.SwipeNum <= 0 {
+		c.JSON(400, gin.H{
+			"error": "Out of swipes",
+		})
+
+		return true
+	}
+
+	var existingRSwipe models.RSwipe
+	config.DB.Where("user_id = ? AND r_swipe_id = ?", userID, swipedID).First(&existingRSwipe)
+
+	if existingRSwipe.UserID != 0 {
+		c.JSON(400, gin.H{
+			"error": "Already swiped",
+		})
+
+		return true
+	}
+
+	var existingLSwipe models.LSwipe
+	config.DB.Where("user_id = ? AND l_swipe_id = ?", userID, swipedID).First(&existingLSwipe)
+
+	if existingLSwipe.UserID != 0 {
+		c.JSON(400, gin.H{
+			"error": "Already swiped",
+		})
+
+		return true
+	}
+	return false
+}
+
 func Left(c *gin.Context) {
 	var swipe models.LSwipe
 	c.ShouldBindJSON(&swipe)
@@ -84,44 +92,8 @@ func Left(c *gin.Context) {
 
 	var swiper models.User
 	config.DB.First(&swiper, userID)
-	var existingUser models.User
-	config.DB.First(&existingUser, swipe.LSwipeID)
 
-	if existingUser.ID == 0 {
-		c.JSON(400, gin.H{
-			"error": "User not found",
-		})
-
-		return
-	}
-
-	if swiper.SwipeNum <= 0 {
-		c.JSON(400, gin.H{
-			"error": "Out of swipes",
-		})
-
-		return
-	}
-
-	var existingRSwipe models.RSwipe
-	config.DB.Where("user_id = ? AND r_swipe_id = ?", userID, swipe.LSwipeID).First(&existingRSwipe)
-
-	if existingRSwipe.UserID != 0 {
-		c.JSON(400, gin.H{
-			"error": "Already swiped",
-		})
-
-		return
-	}
-
-	var existingLSwipe models.LSwipe
-	config.DB.Where("user_id = ? AND l_swipe_id = ?", userID, swipe.LSwipeID).First(&existingLSwipe)
-
-	if existingLSwipe.UserID != 0 {
-		c.JSON(400, gin.H{
-			"error": "Already swiped",
-		})
-
+	if isSwiped(userID, swipe.LSwipeID, swiper, c) {
 		return
 	}
 
