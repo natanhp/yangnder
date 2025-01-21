9b7df1b7d98123d54b64a314a02768bc786b6c47
Natan Hari Pamungkas <natanhp@protonmail.com>
2025-01-21 13:39:42 +0700

    Implement swipe r
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- controllers/swipes.go
+++ controllers/swipes.go
@@ -4,12 +4,14 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/natanhp/yangnder/auth"
 	"github.com/natanhp/yangnder/config"
 	"github.com/natanhp/yangnder/models"
 )
 
 func SwipeRoutes(route *gin.Engine) {
-	swipe := route.Group("/swipes")
+	swipe := route.Group("/swipes", auth.AuthenticateMiddleware)
 	swipe.POST("/right", right)
 	swipe.POST("/left", left)
 }
@@ -17,25 +19,48 @@ func SwipeRoutes(route *gin.Engine) {
 func right(c *gin.Context) {
 	var swipe models.RSwipe
 	c.ShouldBindJSON(&swipe)
-	// Todo: Get user from token
+	claims := c.MustGet("claims").(jwt.MapClaims)
+	userID := uint(claims["sub"].(float64))
 
 	var existingUser models.User
-	config.DB.First(&existingUser, swipe.UserID)
+	config.DB.First(&existingUser, uint(userID))
 
 	if existingUser.ID == 0 {
 		c.JSON(400, gin.H{
 			"error": "User not found",
 		})
+
+		return
 	}
 
 	if existingUser.SwipeNum <= 0 {
 		c.JSON(400, gin.H{
 			"error": "Out of swipes",
 		})
+
+		return
 	}
+
+	var existingSwipe models.RSwipe
+	config.DB.Where("user_id = ? AND r_swipe_id = ?", userID, swipe.RSwipeID).First(&existingSwipe)
+
+	if existingSwipe.UserID != 0 {
+		c.JSON(400, gin.H{
+			"error": "Already swiped",
+		})
+
+		return
+	}
+
+	swipe.UserID = userID
 
 	config.DB.Create(&swipe)
 	config.DB.Model(&existingUser).Update("swipe_num", existingUser.SwipeNum-1)
+
+	c.JSON(201, gin.H{
+		"data": swipe,
+		"user": existingUser,
+	})
 }
 
 func left(c *gin.Context) {
