2201ad7b1fe1ea2085ba44f1b6ed66aee4b1f38d
Natan Hari Pamungkas <natanhp@protonmail.com>
2025-01-21 20:21:06 +0700

    Create test right already l swiped
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- controllers/swipes_test.go
+++ controllers/swipes_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
@@ -139,6 +140,40 @@ func (suite *SwipesControllerTestSuite) TestRightAlreadyRSwiped() {
 	suite.DB.Create(&models.RSwipe{
 		UserID:   1,
 		RSwipeID: 2,
+	})
+
+	payload := `{ "r_swipe_id": 2 }`
+	req, _ := http.NewRequest(http.MethodPatch, "/swipes/right", strings.NewReader(payload))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+
+	suite.Routes.ServeHTTP(rec, req)
+
+	assert.Equal(suite.T(), http.StatusBadRequest, rec.Code)
+
+	var responseBody map[string]interface{}
+	err := json.Unmarshal(rec.Body.Bytes(), &responseBody)
+	assert.NoError(suite.T(), err)
+
+	data := responseBody["error"].(string)
+	assert.Equal(suite.T(), "Already swiped", data)
+}
+
+func (suite *SwipesControllerTestSuite) TestRightAlreadyLSwiped() {
+	claims := jwt.MapClaims{
+		"sub": float64(1),
+	}
+	suite.Routes.Use(func(c *gin.Context) {
+		c.Set("claims", claims)
+		c.Next()
+	})
+
+	suite.Routes.PATCH("/swipes/right", controllers.Right)
+
+	suite.DB.Create(&models.LSwipe{
+		UserID:   1,
+		LSwipeID: 2,
+		DeleteOn: time.Now(),
 	})
 
 	payload := `{ "r_swipe_id": 2 }`
